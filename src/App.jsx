cb00f50640fe9b9ed260c49f7e6b3067894ac721
Albeirox29 <albeiroricardovalverde000@gmail.com>
2024-05-30 07:08:03 -0500

    Second commit learn react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Button from "./components/Button";
 import "./styles/style.css";
 
 const App = () => {
   return (
     <>
       <h1>Hello world</h1>
+      <Button content="Click Here" color="red"  />
+      <Button content="Click That" color="yellow" />
     </>
   );
 };
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ color, content }) => {
+  return <button style={{ background: color }}>{content}</button>;
+};
+
+export default Button;
